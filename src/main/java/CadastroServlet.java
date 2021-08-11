4cdb2be454e9b9bf01ce48a5b0258af1aaa16091
Luiz Felipe <luizfelipe.freire@gmail.com>
2021-08-10 21:18:34 -0300

    Concluindo o fluxo para gravar dados no DB

--- src/main/java/CadastroServlet.java
+++ src/main/java/CadastroServlet.java
@@ -1,5 +1,8 @@
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -27,12 +30,34 @@ public class CadastroServlet extends HttpServlet {
 		saida.println(dtNascimento);
 		saida.println(email);
 		saida.println(sexo);
-		
+		String lsTecnologia = "";
 		for (String t : tecnologia) {
 			saida.println(t);	
+			lsTecnologia += t+",";
 		}
 		
 		saida.println(escolaridade);
+		
+		try {
+			Class.forName("org.postgresql.Driver");
+			String url = "jdbc:postgresql://chunee.db.elephantsql.com:5432/zvrnaorq";
+			String usuarioDb = "zvrnaorq";
+			String senhaDb = "jLYIKYCxll8vriKyyaFT5D8VtOJG0XDo";
+			Connection cont = DriverManager.getConnection(url, usuarioDb, senhaDb);
+			saida.println("Ok para conexão");
+			
+			String sql = "insert into pessoas (nome_completo , telefone , dt_nascimento , email , sexo , tecnologia , escolaridade ) "
+					+ "values('"+nomeCompleto+"', '"+telefone+"', '"+dtNascimento+"', '"+email+"', '"+sexo+"', '"+lsTecnologia+"', '"+escolaridade+"') ";
+			PreparedStatement pst = cont.prepareStatement(sql);
+			pst.execute();
+			pst.close();
+			cont.close();
+			
+			saida.println("Registro Gravado");
+		} catch (Exception e) {
+			saida.println("Erro de conexão");
+		}
+		
 		saida.println("</html>");
 	}
 
---
+++ src/main/sql/insert-tb-pessoa.sql
@@ -0,0 +1,6 @@
+insert
+	into
+	pessoas (nome_completo , telefone , dt_nascimento , email , sexo , tecnologia , escolaridade )
+values('Luiz Maia', '999333002', '1987-07-21', 'luiz@gmail.com', 'm', '', '');
+
+select * from pessoas p ;
